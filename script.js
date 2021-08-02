1acb22e26e1311ea8e888c70e81b95a8f2c77278
saadarif-14 <saadarif1144@gmail.com>
2021-08-02 10:45:56 +0500

    Finall project after editing

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
 function settingsMenuToggle() {
-    var element = document.getElementById("settings-menu");
-   //console.log(element)
-    element.classList.toggle("show");
-}
-/*function sharePost() { 
-    var share = document.getElementById('post-container');
-    document.getElementById('my-output').textContent = share; 
-    console.log(share);
-
-}*/
-//For commemt section
-function writeComment() {
-    var element = document.getElementById("comment-section");
-   //console.log(element)
-    element.classList.toggle("show");
-    
-
-}
-var commentClick = 12; 
-function getwords() {
-    text = words.value;
-    document.getElementById("para").innerHTML += '<p>'+text
-    document.getElementById("words").value = " "
-    commentClick += 1;
-    document.getElementById('commentClicks').textContent = commentClick;
+  var element = document.getElementById("settings-menu");
+  //console.log(element)
+  element.classList.toggle("show");
 }
 
+function writeComment(event) {
+  event.path[4].childNodes[7].classList.toggle("show");
+}
+var commentClick = 12;
+function getwords(event) {
+  if (event.keyCode == 13 || event.which == 13) {
+    text = event.target.value;
+    console.log(event)
+    event.path[3].childNodes[11].innerHTML += "<p>" + text;
+    // document.getElementById("para").innerHTML += "<p>" + text;
+    //console.log(event.target.value)
+    //event.target.value = "";
+    //  path[3].childNodes[5].childNodes[1].childNodes[3].children[1]. += 1;
+    //document.getElementById("commentClicks").textContent = commentClick;
+  }
+}
 //For share post
 var share = 20;
 function sharePost() {
-    var itm = document.getElementById("post-container");
-    var cln = itm.cloneNode(true);
-    document.getElementById("output").appendChild(cln);
-    share += 1;
-    document.getElementById("shareNumber").textContent = share;
+  var itm = document.getElementById("post-container");
+  var cln = itm.cloneNode(true);
+  document.getElementById("output").appendChild(cln);
+  share += 1;
+  document.getElementById("shareNumber").textContent = share;
 }
 // For like button
 var color1 = document.getElementsByClassName("fa-thumbs-up")[0];
 var clicks = 120;
-function like() {
-    document.getElementById("clicks").innerHTML = clicks;
-    color1.style.color = color1.style.color === 'blue' ? 'black': 'blue';
-    if (color1.style.color == 'blue'){
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-    } else{
-        clicks -= 1;
-        document.getElementById("clicks").innerHTML = clicks;
-    }
-    
+function like(event) {
+  console.log(event.path[1].children[1]);
+  event.path[1].children[1].innerHTML = color1;
+
+  //document.getElementById("clicks").innerHTML = clicks;
+  color1.style.color = color1.style.color === "blue" ? "black" : "blue";
+  if (color1.style.color == "blue") {
+    //clicks += 1;
+    event.path[1].children[1].innerHTML = clicks + 1;
+    //document.getElementById("clicks").innerHTML = clicks;
+  } else {
+    //clicks -= 1;
+    event.path[1].children[1].innerHTML = clicks;
+    //document.getElementById("clicks").innerHTML = clicks;
+  }
 }
-
-
